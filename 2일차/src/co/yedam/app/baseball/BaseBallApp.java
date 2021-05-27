6799357589b0e220112566df4fe85e5adb53e069
admin <admin@YD03-08>
2021-05-27 17:58:09 +0900

    first commit

---
+++ 2일차/src/co/yedam/app/baseball/BaseBallApp.java
@@ -0,0 +1,35 @@
+package co.yedam.app.baseball;
+
+public class BaseBallApp {
+/*
+ * 임의의 수 3개
+ * 사용자 수 입력
+ * 스트라익, 볼 카운터 
+ */
+public class BaseBallApp {
+	
+	int s;
+	int b;
+	int c1,c2,c3;
+	int u1,u2,u3;
+	
+	void makeRnd() {
+		c1 = ___; //랜덤생성
+	}
+	void input() {
+		u1 = //입력
+	}
+	void count() {
+		s,b 카운터 //if구문 이용
+	}
+	void start() {
+		makeRnd();
+		while(s<3) {
+			s=b=0;
+			input();
+			count();
+		}
+	}
+	}
+
+}
