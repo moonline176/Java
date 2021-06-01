2a175184aeab12397f961e7bec9cad7ceaaa43fd
moonl <moonl@192.168.123.186>
2021-06-01 23:39:32 +0900

    commit

--- java_book/src/도서관/MemberService.java
+++ java_book/src/도서관/MemberService.java
@@ -23,43 +23,70 @@ public class MemberService implements Service{
 		System.out.printf("이름>");
 		String name = scanner.next();
 		System.out.print("전화번호>");
-		String Phone = scanner.next();
-		members[cnt++] = new Member(name, Phone); 
+		String phone = scanner.next();
+		members[cnt++] = new Member(name, phone); 
 		System.out.println("등록된 건수:" + cnt);
 	}
 	
 	//수정
+//	public void update() {
+//		System.out.printf("수정할 이름>");
+//		for (int i = 0; i < cnt - 1; i++) {
+//			(members[i].getName()).equals(scanner.next());
+//			if (i >= cnt - 1) {
+//				System.out.println("없는 이름입니다.");
+//				return;
+//			}
+//			System.out.print("전화번호>");
+//			String Phone = scanner.next();
+//			members[i].setPhone(Phone);
+//			System.out.println("수정되었습니다.");
+//		}
+//	}
+	
 	public void update() {
-		System.out.printf("수정할 이름>");
-		for (int i = 0; i < cnt - 1; i++) {
-			(members[i].getName()).equals(scanner.next());
-			if (i >= cnt - 1) {
-				System.out.println("없는 이름입니다.");
-				return;
-			}
-			System.out.print("전화번호>");
-			String Phone = scanner.next();
-			members[i].setPhone(Phone);
-			System.out.println("수정되었습니다.");
+		System.out.printf("수정할 번호>");
+		int idx = scanner.nextInt();
+		if(idx >= cnt) {
+			System.out.println("없는 번호입니다.");
+			return;
 		}
+		System.out.print("전화번호>");
+		String phone = scanner.next();
+		members[idx].setPhone(phone); 
+		System.out.println("수정되었습니다.");
 	}
 
 	// 삭제
 	public void delete() {
-		System.out.printf("삭제할 회원>");
-		for (int i = 0; i < cnt - 1; i++) {
-			(members[i].getName()).equals(scanner.next()); // 문자열 비교 수정필요
-			if (i > cnt - 1) {
-				System.out.println("없는 회원입니다.");
-				return;
-			}
-			for (int j = 0; j < cnt - 1; j++) {
-				members[j] = members[j + 1];
-			}
+		System.out.printf("삭제할 번호>");
+		int idx = scanner.nextInt();
+		if(idx > cnt-1) {
+			System.out.println("없는 번호입니다.");
+			return;
+		}
+		for(int i=idx; i<cnt-1; i++) {
+			members[i] = members[i+1];
 		}
 		cnt--;
 		System.out.println("삭제되었습니다.");
 	}
+	
+//	public void delete() {
+//		System.out.printf("삭제할 회원>");
+//		for (int i = 0; i < cnt - 1; i++) {
+//			(members[i].getName()).equals(scanner.next()); // 문자열 비교 수정필요
+//			if (i > cnt - 1) {
+//				System.out.println("없는 회원입니다.");
+//				return;
+//			}
+//			for (int j = 0; j < cnt - 1; j++) {
+//				members[j] = members[j + 1];
+//			}
+//		}
+//		cnt--;
+//		System.out.println("삭제되었습니다.");
+//	}
 	
 	//전체조회
 	public void selectAll() {
@@ -72,18 +99,28 @@ public class MemberService implements Service{
 	}
 	
 	//단건조회(타이틀)
+//	public void selectOne() {
+//		System.out.println("조회할 이름>");
+//		for (int i = 0; i < cnt ; i++) {
+//			(members[i].getName()).equals(scanner.next());
+//			if(i >= cnt){
+//				System.out.println("없는 회원입니다.");
+//				return;
+//			}
+//			System.out.println(String.format("이름:%s   전화번호:%s", members[i].getName(), members[i].getPhone()));// 배열에 있는 이름과 비교
+//			
+//		}
+//
+//	}
+	
 	public void selectOne() {
-		System.out.println("조회할 이름>");
-		for (int i = 0; i < cnt ; i++) {
-			(members[i].getName()).equals(scanner.next());
-			if(i >= cnt){
-				System.out.println("없는 회원입니다.");
-				return;
-			}
-			System.out.println(String.format("이름:%s   전화번호:%s", members[i].getName(), members[i].getPhone()));// 배열에 있는 이름과 비교
-			
+		System.out.printf("조회할 번호>");
+		int idx = scanner.nextInt()-1;
+		if(idx > cnt) {
+			System.out.println("없는 번호입니다.");
+			return;
 		}
-
+		System.out.println(String.format("회원:%s   전화번호:%s", members[idx].getName(), members[idx].getPhone()));
 	}
 		
 
